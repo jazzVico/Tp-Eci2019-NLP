9da32e2aad99001343b9b6c821345784d738d1f0
Horacio Gomez <horaciogomez.1993@gmail.com>
2019-08-08 20:37:40 -0300

    agrego algo que habia borrado accidentalmente

--- tp.py
+++ tp.py
@@ -19,17 +19,16 @@ def main():
   _ws = 6
   _epochs = 3
   model = fasttext.train_supervised(input='fasttext_train_data.preprocessed.txt', epoch=_epochs, lr=_lr, ws=_ws, loss="softmax", wordNgrams=3)# dim=300, pretrainedVectors="../fasttext_vectors/crawl-300d-2M.vec",)
-  (N, p, r) =  model.test('fasttext_dev_data.preprocessed.txt')
-  print_results(N,p,r)
-    
-    #save_data(file,_epochs,_ws,_lr,p,r)
-  #print(" ")
+  #(N, p, r) =  model.test('fasttext_dev_data.preprocessed.txt')
+  #print_results(N,p,r)
+  #save_data(file,_epochs,_ws,_lr,p,r)
   
-#      f=open("test.txt","r")
-#      sentences = f.read().split("\n")
-#      for sentence in sentences:
-#          prediction = model.predict(sentence)
-#          fres.write(prediction[0][0]+"\n")
+  fres=open("testRes.txt","w")
+  f=open("test.txt","r")
+  sentences = f.read().split("\n")
+  for sentence in sentences:
+      prediction = model.predict(sentence)
+      fres.write(prediction[0][0]+"\n")
 
 def print_results(N, p, r):
     print("N\t" + str(N))
