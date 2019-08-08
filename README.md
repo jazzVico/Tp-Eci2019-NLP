a424f88c57cd88e756f8cb2013e12a1eb39c4434
jazzVico <jazminalvarezvico@gmail.com>
2019-08-08 03:09:55 -0300

    clasificador q parece hacer algo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Tp de la eci 2019 NLP
+
+para generar el txt que es lo q se utiliza para generar el modelo (entiendo q el modelo final porque lo estamos entrenando con el corpus + labels)
+correr python3 read_data.py snli_1.0_train_filtered.jsonl snli_1.0_train_gold_labels.csv >pepe.txt 
+despes correr tp.py
+si lo miras x ahora solo tiene una linea. 
+tambien esta la opcion de hacerlo no supervisado, habria q ver si nos interesa para algo?
+
+segui el tutorial que te paso sol, lo siguiente q dice es como testearlo. 
+no se si hay q hacer mucho mas...
+
+y hay q hacer un mini informe(???)
+
+
+(tomate la libertad de cambiarle el nombre al archivo si te place)
--- tp.py
+++ tp.py
@@ -5,19 +5,24 @@ import torch.nn.functional as F
 import torch.optim as optim
 import fasttext
 import io
+import argparse 
+
 
 torch.manual_seed(42)
 
-def load_vectors(fname):
-    i=0
-    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    n, d = map(int, fin.readline().split())
-    data = {}
-    for line in fin:
-        tokens = line.rstrip().split(' ')
-        data[tokens[0]] = map(float, tokens[1:])
-        i+=1
-        if i==100:      
-            return data
+# def load_vectors(fname):
+#     i=0
+#     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+#     n, d = map(int, fin.readline().split())
+#     data = {}
+#     for line in fin:
+#         tokens = line.rstrip().split(' ')
+#         data[tokens[0]] = map(float, tokens[1:])
+#         i+=1
+#         if i==100:      
+#             return data
 
-vectors = load_vectors("wiki-news-300d-1M.vec")
+# vectors = load_vectors("wiki-news-300d-1M.vec")
+
+
+model = fasttext.train_supervised('pepe.txt')
