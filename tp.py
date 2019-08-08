7a96aac433ef36cc36858366389a7023d19f93c6
jazzVico <jazminalvarezvico@gmail.com>
2019-08-08 10:12:40 -0300

    script entrenamiento+validacion (tp.py)

--- tp.py
+++ tp.py
@@ -1,28 +1,20 @@
-import torch
-import torch.autograd as autograd
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+
 import fasttext
 import io
 import argparse 
 
 
-torch.manual_seed(42)
 
-# def load_vectors(fname):
-#     i=0
-#     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-#     n, d = map(int, fin.readline().split())
-#     data = {}
-#     for line in fin:
-#         tokens = line.rstrip().split(' ')
-#         data[tokens[0]] = map(float, tokens[1:])
-#         i+=1
-#         if i==100:      
-#             return data
+def main():
 
-# vectors = load_vectors("wiki-news-300d-1M.vec")
+    model = fasttext.train_supervised('pepe.txt', epoch=50, lr=0.4, ws=10)
+    print_results(*model.test('validacion.txt'))
+
+def print_results(N, p, r):
+    print("N\t" + str(N))
+    print("P@{}\t{:.3f}".format(1, p))
+    print("R@{}\t{:.3f}".format(1, r))
 
 
-model = fasttext.train_supervised('pepe.txt')
+
+main()
